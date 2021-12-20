f9bbff36da2a89bab0ae7ebb1b5a87aa18e9d179
Александр Черноев <grvr.acc@gmail.com>
2021-12-21 00:36:48 +0300

    Добавьте файлы проекта.

---
+++ Models/Machines.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Machines
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreationDate { get; set; }
+        public string Class { get; set; }
+        public decimal Price { get; set; }
+    }
+}
